35828b8a3818a4206b125b0ff4f6754085bde523
Divith123 <divithselvam23@gmail.com>
2025-07-14 20:45:46 +0530

    Base

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to OneDot E-Commerce!</h1>
+      <p>This is the landing page. Explore our products and offers.</p>
+    </div>
+  )
+}
+
+export default Landing
