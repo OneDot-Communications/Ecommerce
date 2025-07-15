94dcb49be96fea63cb5a7b33efec7455b9e974e5
Nivethitha-1131 <nivethitha1131@gmail.com>
2025-07-15 20:09:44 +0530

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Landing from './pages/Home/Home'
+import Landing from './pages/Home/signup'
 import './App.css'
 
 function App() {
