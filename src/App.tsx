2a4897cc102a02191857d482c162fa99762ce8b3
unknown <you@example.com>
2025-07-15 19:55:25 +0530

    Modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Landing from './pages/Home/Home'
+import About from './pages/Home/About'
 import './App.css'
 
 function App() {
@@ -7,9 +9,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/About" element={<About />} />
       </Routes>
     </Router>
   )
 }
 
 export default App
+
