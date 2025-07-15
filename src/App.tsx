5cd227c1a8db02f7c471a613557f58bd6b316b8c
ShamGanesh27 <shamcurry3030@gmail.com>
2025-07-15 19:52:33 +0530

    chamges

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Landing from './pages/Home/Home'
+import Profile from './pages/Home/Profile'
 import './App.css'
-
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/Profile" element={<Profile />} />
       </Routes>
     </Router>
   )
