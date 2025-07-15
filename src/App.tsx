5308340fac8b5751ba3731aa6755d350ad57be54
Divith123 <divithselvam23@gmail.com>
2025-07-15 20:31:41 +0530

    Fixed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Landing from './pages/Home/Home'
 import Profile from './pages/Home/Profile'
 import './App.css'
-
+import Signup from './pages/Home/signup'
 import About from './pages/Home/About'
 import Dashboard from './pages/Dashboard'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TermsAndConditions from './pages/Home/TermsAndConditions';
 import Home from './pages/Home/Home';
 
@@ -20,6 +19,7 @@ function App() {
         <Route path="/About" element={<About />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
       </Routes>
     </Router>
